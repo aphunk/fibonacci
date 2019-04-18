b3dd33fc95484cb95cc9980f02b37809a341e110
Amy Phung <phung.amy85@gmail.com>
2019-04-17 20:50:00 -0700

    wrote fibonacci method

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,12 +1,21 @@
-# Computes the nth fibonacci number in the series starting with 0.
-# fibonacci series: 0 1 1 2 3 5 8 13 21 ...
-# e.g. 0th fibonacci number is 0
-# e.g. 1st fibonacci number is 1
-# ....
-# e.g. 6th fibonacci number is 8
-
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) where n is the input
+# Space complexity: O(1)
 def fibonacci(n)
-  raise NotImplementedError
+  if n == nil || n < 0
+    raise ArgumentError, "enter a valid number"
+  end
+
+  if n == 0 || n == 1
+    return n
+  else
+    a = 0
+    b = 1
+
+    n.times do
+      temp = a
+      a = b
+      b = temp + b
+    end
+  end
+  return a
 end
